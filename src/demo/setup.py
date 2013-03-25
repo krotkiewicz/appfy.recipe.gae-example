0f13f90a53c3361bed7cfd09e62f4e5700e6b7a2
Konrad Rotkiewicz <konrad.rotkiewicz@stxnext.pl>
2013-03-25 07:47:11 +0100

    init

---
+++ src/demo/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+name = "demo"
+version = "0.1"
+
+dependency_links=[
+]
+
+setup(
+    name=name,
+    version=version,
+    description="demo",
+    long_description='',
+    classifiers=[],
+    keywords="",
+    author="",
+    author_email='',
+    url='',
+    license='',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    dependency_links=dependency_links,
+    install_requires=[
+        'markdown',
+        'httplib2',
+        'apiclient',
+        'wtforms',
+    ],
+)
